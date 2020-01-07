f1d4da9a4a73a2719030024e2e7f80802a2ba50d
Félix <felix.sotelo.q@outlook.es>
2020-01-07 16:29:38 -0300

    RestServer tercer Commit

--- server/config/config.js
+++ server/config/config.js
@@ -6,10 +6,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 /**BASE DE DATOS */
 let urlDB;
 
-//if (process.env.NODE_ENV === 'dev') {
-//    urlDB = 'mongodb://localhost:27017/my_cafe';
-//}else{
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/my_cafe';
+}else{
     urlDB = 'mongodb+srv://bjorskull:F3kpCLON002-@cluster0-co4tj.mongodb.net/my_cafe'
-//}
+}
 
 process.env.URLDB = urlDB;
